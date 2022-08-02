852011514488a90f7314af21bc7f5f567462ef1f
idrissa Dabo <45718374+idiise@users.noreply.github.com>
2022-08-02 17:47:46 +0000

    Finalisation du Calcul du SCA

--- syntax/syntax 2018.R
+++ syntax/syntax 2018.R
@@ -10,7 +10,33 @@ library(writexl)
 
 # import datasets ---------------------------------------------------------
 
-here::here("data/Baseline 2018/")
+setwd("C:/Users/USER MSI/Documents/R Project/Lessonlearnt2022/data/Baseline 2018/")
 
-List_data <- dir_ls(regexp = here::here("data/Baseline 2018/.sav"))
-here::set_here("data/Baseline 2018/")
+List_data <- dir_ls(regexp = ".sav") %>% 
+  map(read_sav)
+  nom_base <- names(List_data)
+  nom_base <- str_replace(string = nom_base,pattern = ".sav", replacement = "")
+  names(List_data) <- nom_base
+  list2env(List_data, .GlobalEnv)
+BaseMenageNER_2018 <- read_sav("C:/Users/USER MSI/Documents/R Project/Lessonlearnt2022/data/BaseMenageNER_2018.sav",
+                               encoding="latin1")
+
+
+# Burkina Faso ------------------------------------------------------------
+
+BaseMenageBFA_2018<- to_factor(BaseMenageBFA_2018)
+codebook_BFA2018 <- var_label(BaseMenageBFA_2018)
+codebook_BFA2018 <- as.data.frame(do.call(rbind,codebook_BFA2018))
+codebook_BFA2018 <- codebook_BFA2018 %>% rownames_to_column()
+
+write_xlsx(codebook_BFA2018, "C:/Users/USER MSI/Documents/R Project/Lessonlearnt2022/Codebook/codebook_BFA2018.xlsx")
+
+
+# Mali --------------------------------------------------------------------
+
+BaseMenageBMZ_MLI2018<- to_factor(BaseMenageBMZ_MLI2018)
+codebook_MLI2018 <- var_label(BaseMenageBMZ_MLI2018)
+codebook_MLI2018 <- as.data.frame(do.call(rbind,codebook_MLI2018))
+codebook_MLI2018 <- codebook_MLI2018 %>% rownames_to_column()
+
+write_xlsx(codebook_MLI2018, "C:/Users/USER MSI/Documents/R Project/Lessonlearnt2022/Codebook/codebook_MLI2018.xlsx")
